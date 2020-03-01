93973964527cbd668e083662932bcdc9e581be49
Anna <anna@Annas-MacBook-Pro.local>
2020-03-01 18:06:27 +0200

    first commit

---
+++ javascripting/looping-through-arrays.js
@@ -0,0 +1,6 @@
+let pets = ["cat", "dog", "rat"];
+let len = pets.length;
+for (let i = 0; i < len; i++) {
+  pets[i] = pets[i] + "s";
+}
+console.log(pets);
---
+++ javascripting/string-length.js
@@ -0,0 +1,6 @@
+let example = "example string";
+let length = example.length;
+console.log(length);
+
+// const example = 'example string'
+// console.log(example.length)
