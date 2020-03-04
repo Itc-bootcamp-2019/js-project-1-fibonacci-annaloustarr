733c3def3d6fae781817b2ccfd19a423fa2ded19
Anna Starr <anna.starr@gmail.com>
2020-03-04 16:23:21 +0200

    Completed milestone 5

--- js/main.js
+++ js/main.js
@@ -12,28 +12,37 @@ let x;
 //     document.getElementById("Y").innerHTML = y;
 //   }
 // }
-function fibRecursive(x) {
-  if (x < 2) return x;
-  return fibRecursive(x - 1) + fibRecursive(x - 2);
+// function fibRecursive(x) {
+//   if (x < 2) return x;
+//   return fibRecursive(x - 1) + fibRecursive(x - 2);
+// }
+
+function getFibonacci(x) {
+  fetch(`http://localhost:5050/fibonacci/${x}`)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      let y = data.result;
+      console.log(y);
+      document.getElementById("Y").innerHTML = y;
+    })
+    .catch(error => {
+      console.log("error!");
+      console.error(error);
+    });
 }
+
+// async function getFibonacci(x) {
+//   const response = await fetch(`http://localhost:5050/fibonacci/:number`);
+//   const y = await response.y();
+//   console.log(y);
+//   document.getElementById("Y").innerHTML = y;
 
 function getX() {
   let x = document.getElementById("X").value;
-  document.getElementById("Y").innerHTML = fibRecursive(x);
-  // fibonacci(x);
+  document.getElementById("Y").innerHTML = getFibonacci(x);
   console.log(x);
 }
 document.getElementById("myButton").addEventListener("click", getX);
-
-// function giveY() {
-//   document.getElementById("Y").innerHTML = y;
-// }
-// document.getElementById("myButton").addEventListener("click", giveY);
-
-// X.innerText = x;
-// Y.innerText = y;
-
-// let x = prompt("Enter a value for X", 0);
-// document.write(first + ", ");
-// document.write(second + ", ");
-// document.write(y);
